58b0fa04fdc3bf022ceb375caca1f21028b1fe30
FREDDY-OWOLABI <freddydohou20@gmail.com>
2025-11-13 13:05:09 +0000

    Mise à jour du 13/11/2025 13h04

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -33,13 +33,13 @@ const Support = () => {
           <h1 className="text-3xl font-bold font-heading mb-6 w-full text-center">
             Pourquoi ?
           </h1>
-          <h2 className=" guton mb-4">
+          <h2 className=" guton mb-4 text-xl">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </h2>
-          <p className="leading-relaxed mb-4">
+          <p className="guton mb-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
-          <p className=" leading-relaxed">
+          <p className=" guton">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
